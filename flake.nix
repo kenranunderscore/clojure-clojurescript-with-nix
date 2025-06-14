b341db1da4c66adf3a552f2004e92f5854429ed0
Johannes Maier <johannes.maier@mailbox.org>
2025-06-14 20:42:50 +0200

    configure clj, shadow and maven to use local directories

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         { system, inputs', ... }:
         let
           pkgs = inputs'.nixpkgs.legacyPackages;
-          npmRoot = ./the-app;
+          appRoot = "the-app";
         in
         {
           formatter = pkgs.nixfmt-rfc-style;
@@ -33,17 +33,25 @@
             packages = [
               pkgs.clojure
               pkgs.nodejs
-              # This works as long as there's no custom `shellHook`, but you may
-              # just call it manually in that case
+              # This would work on its own as long as there's no custom
+              # `shellHook`, but you may just call it manually in that case as
+              # below.
               pkgs.importNpmLock.hooks.linkNodeModulesHook
             ];
+            shellHook = ''
+              echo [shell setup] preparing environment...
+              linkNodeModulesHook
+              export GITLIBS=$(pwd)/${appRoot}/.gitlibs
+              export CLJ_CONFIG=$(pwd)/${appRoot}/.clj
+              echo [shell setup] done
+            '';
             # Set `npmRoot` here so that the node_modules link will be created
             # in the correct subdirectory. Also assumes that `nix develop` is
             # called from the root/flake directory (which, for instance,
             # `direnv` does by default).
-            npmRoot = builtins.baseNameOf npmRoot;
+            npmRoot = appRoot;
             npmDeps = pkgs.importNpmLock.buildNodeModules {
-              inherit npmRoot;
+              npmRoot = ./. + "/${appRoot}";
               inherit (pkgs) nodejs;
             };
           };
