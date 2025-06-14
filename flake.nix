9d157e693886f3696aae50522b00e66437aa1c00
Johannes Maier <johannes.maier@mailbox.org>
2025-06-14 18:29:52 +0200

    manage node_modules with nix via `importNpmLock`

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
         { system, inputs', ... }:
         let
           pkgs = inputs'.nixpkgs.legacyPackages;
+          npmRoot = ./the-app;
         in
         {
           formatter = pkgs.nixfmt-rfc-style;
@@ -32,7 +33,19 @@
             packages = [
               pkgs.clojure
               pkgs.nodejs
+              # This works as long as there's no custom `shellHook`, but you may
+              # just call it manually in that case
+              pkgs.importNpmLock.hooks.linkNodeModulesHook
             ];
+            # Set `npmRoot` here so that the node_modules link will be created
+            # in the correct subdirectory. Also assumes that `nix develop` is
+            # called from the root/flake directory (which, for instance,
+            # `direnv` does by default).
+            npmRoot = builtins.baseNameOf npmRoot;
+            npmDeps = pkgs.importNpmLock.buildNodeModules {
+              inherit npmRoot;
+              inherit (pkgs) nodejs;
+            };
           };
         };
     };
